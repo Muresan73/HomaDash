6ba56ed51e90611576a82db6fad42d8edb2d7d42
e-moG <e-mog@MacBook-Pro.local>
2018-03-05 13:36:02 +0100

    Basic

---
+++ server/routes/api.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+// Get users
+router.get('/test', (req, res) => {
+    console.log("sent data: alma");
+    response.data = ["alma"]
+    res.json(response);
+});
+
+module.exports = router;
